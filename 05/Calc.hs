14a148fdd6b08f4c99eacb9e4fe75e933d848c1a
hask <me+fpcomplete@dirkz.com>
2015-01-23 01:45:16 -0800

    week 05, exercise 4

--- 05/Calc.hs
+++ 05/Calc.hs
@@ -29,3 +29,28 @@ instance Expr ExprT where
     lit = Lit
     mul = Mul
     add = Add
+
+instance Expr Integer where
+    lit = id
+    mul a b = a * b
+    add a b = a + b
+
+instance Expr Bool where
+    lit a
+        | a <= 0 = False
+        | otherwise = True
+    add = (||)
+    mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+    lit = MinMax
+    add (MinMax a) (MinMax b) = MinMax $ max a b
+    mul (MinMax a) (MinMax b) = MinMax $ min a b
+
+instance Expr Mod7 where
+    lit a = Mod7 $ mod a 7
+    add (Mod7 a) (Mod7 b) = Mod7 $ mod a 7 + mod b 7
+    mul (Mod7 a) (Mod7 b) = Mod7 $ mod a 7 * mod b 7
--- 05/Main.hs
+++ 05/Main.hs
@@ -5,12 +5,24 @@ import ExprT
 import Calc
 import Parser
 
+-- exercise 4
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3*-4) + 5"
+
 -- | The main entry point.
 main :: IO ()
 main = do
     putStrLn "CIS-194 Week 05"
+    -- exercise 1
     print $ eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20
+    -- exercise 2
     print $ evalStr "(2+3)*4" == Just 20
     print $ evalStr "2+3*4" == Just 14
     print $ evalStr "2+3*4+" == Nothing
+    -- exercise 4
     print $ (mul (add (lit 2) (lit 3)) (lit 4) :: ExprT) == Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+    print (testExp :: Maybe Integer)
+    print (testExp :: Maybe Bool)
+    print (testExp :: Maybe MinMax)
+    print (testExp :: Maybe Mod7)
