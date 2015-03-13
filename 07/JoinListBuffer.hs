8ddbdec6dc2f83c83ecd42543d4b1cadbb9c222c
Dirk Zimmermann <me@dirkz.com>
2015-03-13 15:04:18 +0100

    week 07, exercise 4 (replaceLine is not correct)

---
+++ 07/JoinListBuffer.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+
+module JoinListBuffer where
+
+import Data.Monoid
+import Data.List (foldl')
+import Buffer
+import JoinList
+import Scrabble
+import Sized
+
+instance Buffer (JoinList (Score, Size) String) where
+    toString Empty = ""
+    toString (Single _ s) = s
+    toString (Append _ left right) = toString left ++ toString right
+
+    fromString = (foldl' addLine Empty) . lines
+        where addLine acc line =
+                acc +++ Single (scoreString line, Size 1) line
+
+    line = indexJ
+
+    replaceLine n s Empty = Single (scoreString s, Size 0) s
+    replaceLine n s orig@(Single m _)
+        | n == 0 = Single (scoreString s, snd m) s
+        | otherwise = orig
+    replaceLine n s orig@(Append _ left right)
+        | n < sizeLeft =
+            let
+                newLeft = replaceLine n s left
+                newTag = (tag newLeft) <> (tag right)
+            in Append newTag newLeft right
+        | n < sizeRight =
+            let
+                newRight = replaceLine (n - sizeLeft) s right
+                newTag = (tag left) <> (tag newRight)
+            in Append newTag left newRight 
+        | otherwise = orig
+        where
+            sizeLeft = getSize $ snd $ tag left
+            sizeRight = getSize $ snd $ tag right
+
+    numLines Empty = 0
+    numLines (Single _ _) = 1
+    numLines (Append _ l r) = numLines l + numLines r
+
+    value Empty = 0
+    value (Single t _) = getScore $ fst t
+    value (Append t _ _) = getScore $ fst t
--- 07/Main.hs
+++ 07/Main.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+
 import Data.Monoid
 import Control.Monad
 import JoinList
 import Sized
+import JoinList
+import Scrabble
+import JoinListBuffer
+import Editor
+import Buffer
 
 sampleJList1 :: JoinList Size String
 sampleJList1 = (Append 4
@@ -27,13 +34,20 @@ checkTake :: (Sized m, Eq a, Monoid m) => JoinList m a -> Int -> Bool
 checkTake l n = jlToList (takeJ n l) == take n (jlToList l)
 
 main = do
-    putStrLn "JoinList tests"
-    forM_ [1..5] $ print . checkLookup  sampleJList1
-    forM_ [1..8] $ print . checkLookup  sampleJList2
-    forM_ [0..2] $ print . checkDrop (Empty :: JoinList Size String)
-    forM_ [0..2] $ print . checkDrop (Single (Size 1) "0")
-    forM_ [0..5] $ print . checkDrop sampleJList1
-    forM_ [0..8] $ print . checkDrop sampleJList2
-    forM_ [0..6] $ print . checkTake sampleJList1
-    forM_ [0..8] $ print . checkTake sampleJList2
-    print $ scoreLine "yay " +++ scoreLine "haskell!"
+    --putStrLn "JoinList tests"
+    --forM_ [1..5] $ print . checkLookup  sampleJList1
+    --forM_ [1..8] $ print . checkLookup  sampleJList2
+    --forM_ [0..2] $ print . checkDrop (Empty :: JoinList Size String)
+    --forM_ [0..2] $ print . checkDrop (Single (Size 1) "0")
+    --forM_ [0..5] $ print . checkDrop sampleJList1
+    --forM_ [0..8] $ print . checkDrop sampleJList2
+    --forM_ [0..6] $ print . checkTake sampleJList1
+    --forM_ [0..8] $ print . checkTake sampleJList2
+    --print $ scoreLine "yay " +++ scoreLine "haskell!"
+    let buffer = fromString "Line 1\nLine 2\nLine 3" :: JoinList (Score, Size) String
+    print $ buffer
+    --print $ line 1 buffer
+    --print $ replaceLine 0 "hi" (Single (Score 0, Size 0) "what")
+    --print $ replaceLine 0 "hi" (Empty :: JoinList (Score, Size) String)
+    --print $ replaceLine 1 "hi" (Single (Score 0, Size 0) "what")
+    --runEditor editor buffer
