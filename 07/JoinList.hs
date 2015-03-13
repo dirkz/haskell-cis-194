f60e68f839674929f512c7967cd86712c777cae6
hask <me+fpcomplete@dirkz.com>
2015-03-13 04:28:53 -0700

    week 07, exercise 2

--- 07/JoinList.hs
+++ 07/JoinList.hs
@@ -2,7 +2,6 @@ module JoinList where
 
 import Data.Monoid
 import Sized
-import Debug.Trace (trace)
 
 data JoinList m a = Empty
     | Single m a
@@ -17,8 +16,11 @@ tag (Single m _) = m
 tag (Append m _ _) = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
-(+++) left@(Single m1 _) right@(Single m2 _) =
-    Append (m1 <> m2) left right
+l +++ Empty = l
+Empty +++ l = l
+left +++ right = Append (m1 <> m2) left right
+    where m1 = tag left
+          m2 = tag right
 
 -- exercise 2
 
@@ -36,8 +38,7 @@ jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
 getSizeInt :: Sized a => a -> Int
 getSizeInt = getSize . size
 
-indexJ :: (Show a, Show b, Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ i l | trace ("indexJ " ++ show i ++ " " ++ show l) False = undefined
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ n (Single m a)
     | n == 0 = Just a
@@ -49,9 +50,30 @@ indexJ n (Append m l1 l2)
             s2 = getSizeInt $ tag l2
         in
             if n < s1 then indexJ n l1
-            else if n < (s1 + s2) then indexJ n l2 else Nothing
+            else if n < (s1 + s2) then indexJ (n - s1) l2
+                    else Nothing
 
-sampleJList :: JoinList Size Char
-sampleJList = (Append 4
-                (Append 2 (Single 1 'a') (Single 1 'b'))
-                (Append 2 (Single 1 'c') (Single 1 'd')))
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty = Empty
+dropJ n s@(Single _ _)
+    | n == 0 = s
+    | otherwise = Empty
+dropJ n (Append m l1 l2)
+    | n >= sizeM = Empty
+    | n == sizeL1 = l2
+    | n < sizeL1 = (dropJ n l1) +++ l2
+    | otherwise  = (dropJ (n - sizeL1) l2) +++ Empty
+    where sizeM = getSizeInt m
+          sizeL1 = getSizeInt $ tag l1
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty = Empty
+takeJ n l | n <= 0 = Empty
+takeJ n s@(Single _ _) | n >= 1 = s
+takeJ n all@(Append m l1 l2)
+    | n >= sizeM = all
+    | n == sizeL1 = l1
+    | n > sizeL1 = l1 +++ (takeJ (n - sizeL1) l2)
+    | n < sizeL1 = takeJ n l1
+    where sizeM = getSizeInt m
+          sizeL1 = getSizeInt $ tag l1
--- 07/Main.hs
+++ 07/Main.hs
@@ -1,6 +1,38 @@
+import Data.Monoid
+import Control.Monad
 import JoinList
 import Sized
 
+sampleJList1 :: JoinList Size String
+sampleJList1 = (Append 4
+                (Append 2 (Single 1 "0") (Single 1 "1"))
+                (Append 2 (Single 1 "2") (Single 1 "3")))
+
+sampleJList2 :: JoinList Size String
+sampleJList2 = (Append 8
+                (Append 6
+                    (Append 4
+                        (Append 2 (Single 1 "0") (Single 1 "1"))
+                        (Append 2 (Single 1 "2") (Single 1 "3")))
+                    (Append 2 (Single 1 "4") (Single 1 "5")))
+                (Append 2 (Single 1 "6") Empty))
+
+checkLookup :: (Sized m, Eq a, Monoid m) => JoinList m a -> Int -> Bool
+checkLookup l n = (indexJ n l) == (jlToList l !!? n)
+
+checkDrop :: (Sized m, Eq a, Monoid m) => JoinList m a -> Int -> Bool
+checkDrop l n = jlToList (dropJ n l) == drop n (jlToList l)
+
+checkTake :: (Sized m, Eq a, Monoid m) => JoinList m a -> Int -> Bool
+checkTake l n = jlToList (takeJ n l) == take n (jlToList l)
+
 main = do
     putStrLn "JoinList tests"
-    print $ indexJ 0 (Single (Size 1) 'a')
+    forM_ [1..5] $ print . checkLookup  sampleJList1
+    forM_ [1..8] $ print . checkLookup  sampleJList2
+    forM_ [0..2] $ print . checkDrop (Empty :: JoinList Size String)
+    forM_ [0..2] $ print . checkDrop (Single (Size 1) "0")
+    forM_ [0..5] $ print . checkDrop sampleJList1
+    forM_ [0..8] $ print . checkDrop sampleJList2
+    forM_ [0..6] $ print . checkTake sampleJList1
+    forM_ [0..8] $ print . checkTake sampleJList2
